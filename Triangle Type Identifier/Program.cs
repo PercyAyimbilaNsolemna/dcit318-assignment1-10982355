0e58a0a4368f9cd167f90c381361039810f3012d
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2024-06-02 22:06:41 +0000

    Start Triangle Type Identifier.

---
+++ Triangle Type Identifier/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class TriangleTypeIdentifier
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Triangle Type Identifier!");
+        Console.WriteLine("Please enter the lengths of the three sides of the triangle:");
+
+      
+    }
+}
