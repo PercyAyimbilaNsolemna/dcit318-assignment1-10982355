22b743b97f6188b7a3c3f765cba96b8bb267f0b5
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2024-06-02 21:49:33 +0000

    Start Ticket Price Calculator.

---
+++ Ticket Price Calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class TicketPriceCalculator
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Ticket Price Calculator!");
+        Console.Write("Enter your age: ");
+
+     
+    }
+}
